5ed69ce88c67f27bfe39c765f252ac42e318e7a2
Dan Banfield <dandbanfield@gmail.com>
2022-07-02 14:47:50 +0100

    Added colour change methods.

--- ConsoleExtensions/ConsoleExtensions.cs
+++ ConsoleExtensions/ConsoleExtensions.cs
@@ -4,6 +4,27 @@ namespace ConsoleExtensions
 {
     public static class ConsoleExtensions
     {
-        // Implement methods here.
+        #region Properties
+
+        private static ConsoleColor cachedConsoleTextColour;
+        private static ConsoleColor cachedConsoleBackgroundColour;
+
+        #endregion
+
+        /// <summary>
+        /// Sets the colour of the console's text (or foreground).
+        /// </summary>
+        public static void SetConsoleTextColour(ConsoleColor colour)
+        {
+            Console.ForegroundColor = cachedConsoleTextColour = colour;
+        }
+
+        /// <summary>
+        /// Sets the colour of the console's background.
+        /// </summary>
+        public static void SetConsoleBackgroundColour(ConsoleColor colour)
+        {
+            Console.BackgroundColor = cachedConsoleBackgroundColour = colour;
+        }
     }
 }
